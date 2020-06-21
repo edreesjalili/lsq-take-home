a9872ceea3834d6acb250b35d1f8473010dcd48f
Ed Jalili <edreesjalili@gmail.com>
2020-06-21 12:08:40 -0500

    spring init files

---
+++ src/main/java/com/jalili/lsqtakehome/LsqTakeHomeApplication.java
@@ -0,0 +1,13 @@
+package com.jalili.lsqtakehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsqTakeHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsqTakeHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jalili/lsqtakehome/LsqTakeHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalili.lsqtakehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsqTakeHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
